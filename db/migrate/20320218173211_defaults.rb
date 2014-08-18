ddadf5df5267fddd75b24711197183efdb4c6806
Nat Tuck <nat@ferrus.net>
2014-08-18 13:04:26 -0400

    Add invite code to defaults.

--- db/migrate/20320218173211_defaults.rb
+++ db/migrate/20320218173211_defaults.rb
@@ -1,8 +1,12 @@
+require 'secrets'
+
 class Defaults < ActiveRecord::Migration
   def up
     return unless Rails.env.development?
 
     Plan.create!(name: "Default", megs: "5120", usd_per_month: "5.00")
+
+    code = Secrets.get_hex('invite_code', 4)
 
     uu = User.create!(
       email: "admin@example.com", 
@@ -11,6 +15,7 @@ class Defaults < ActiveRecord::Migration
       password: "derpcake",
       password_confirmation: "derpcake",
       auth_key: "48e7f03d0b3e92c4777ed5b8714378a5",
+      invite_code: code,
     )
     uu.save!
  
@@ -21,6 +26,7 @@ class Defaults < ActiveRecord::Migration
       password: "derpcake",
       password_confirmation: "derpcake",
       auth_key: "b5a3eb90249d3fafaf140203d06b1d89",
+      invite_code: code,
     )
     uu.save!
   end
